796d3977f94a83065ee49b1fa3614efd6fed0cd9
eli-zarrabal <elizabethzarrabal@gmail.com>
2017-02-16 19:02:44 -0600

    Actualización crítica
    Se actualizo un error de código Kernel

--- app/src/main/java/itsco/edu/helloword/MainActivity.java
+++ app/src/main/java/itsco/edu/helloword/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, "Hice una pequeña actualización", Toast.LENGTH_SHORT).show();
     }
 
     @Override
