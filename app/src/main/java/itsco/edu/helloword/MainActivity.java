642c276d7ab1ccd2381cdcb07c3f335af4c32e5d
eli-zarrabal <elizabethzarrabal@gmail.com>
2017-02-16 18:57:22 -0600

    Inicio del proyecto
    Es una prueba desde windows para que vean como masca la iguana... De
    lado... xD xD xD

---
+++ app/src/main/java/itsco/edu/helloword/MainActivity.java
@@ -0,0 +1,43 @@
+package itsco.edu.helloword;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this, "Activity Start", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(this,"Activity Restart", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this, "Activity Stop", Toast.LENGTH_SHORT).show();
+    }
+@Override
+protected  void onPause(){
+        super.onPause();
+        Toast.makeText(this, "Activity Pause", Toast.LENGTH_SHORT).show();
+}
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "Activity Destroy", Toast.LENGTH_SHORT).show();
+    }
+}
